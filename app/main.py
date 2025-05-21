5283e9196e4f6cba8120c6588b7b68de368386e4
arthur-chaves <arthurchaves96@gmail.com>
2025-05-21 22:31:18 +0200

    Modifications to implement a .csv and a mood function

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import os
+import sys
+import os
+
 
 st.set_page_config(page_title="Holiday Helper", page_icon="🏞️")
 
@@ -15,7 +18,7 @@ mood = st.selectbox("Como você está se sentindo?", ["Relaxado", "Aventureiro",
 
 st.success(f"Humor selecionado: **{mood}** 🎯")
 
-
+# rodar com streamlit run app/main.py
 
 # Garantir que a pasta 'data' existe
 os.makedirs("data", exist_ok=True)
@@ -27,3 +30,18 @@ log_entry = pd.DataFrame([{
     "humor": mood
 }])
 log_entry.to_csv(log_path, mode='a', header=not os.path.exists(log_path), index=False)
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.recommendation import get_recommendations_by_mood
+
+st.markdown("---")
+st.subheader("Sugestões para o seu dia:")
+
+sugestoes = get_recommendations_by_mood(mood)
+
+if sugestoes:
+    st.info(f"🎬 Filme: {sugestoes['filme']}")
+    st.info(f"🌄 Passeio: {sugestoes['passeio']}")
+else:
+    st.warning("Nenhuma sugestão disponível para esse humor.")
